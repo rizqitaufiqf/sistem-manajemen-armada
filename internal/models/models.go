3b52d0341ca6dd9e124f2042dcd8281b86a8b4c2
rizqi taufiq <rizqitaufiqf@gmail.com>
2025-06-11 00:37:03 +0700

    add vehicle models

---
+++ internal/models/models.go
@@ -0,0 +1,27 @@
+package models
+
+type VehicleLocation struct {
+	VehicleID string  `json:"vehicle_id"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	Timestamp int64   `json:"timestamp"`
+}
+
+type LocationPayload struct {
+	VehicleID string  `json:"vehicle_id"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	Timestamp int64   `json:"timestamp"`
+}
+
+type GeofenceEvent struct {
+	VehicleID string `json:"vehicle_id"`
+	Event     string `json:"event"`
+	Location Location `json:"location"`
+	Timestamp int64 `json:"timestamp"`
+}
+
+type Location struct {
+	Latitude float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
