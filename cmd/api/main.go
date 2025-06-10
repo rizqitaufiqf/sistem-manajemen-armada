fe4c6578ae16cf43dc149e84086c0ae07262801f
rizqi taufiq <rizqitaufiqf@gmail.com>
2025-06-11 06:32:07 +0700

    init postgres and api/main

---
+++ cmd/api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"sistem-manajemen-armada/internal/config"
+	"sistem-manajemen-armada/internal/repository"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	cfg := config.LoadConfig()
+
+	repo, err := repository.NewPostgreSQLRepository(cfg.PostgresHost, cfg.PostgresPort, cfg.PostgresUser, cfg.PostgresPass, cfg.PostgresDB)
+	if err != nil {
+		log.Fatalf("Failed to init PostgreSQL repository: %v", err)
+	}
+	defer repo.DB.Close()
+
+	router := gin.Default()
+
+	// Run the API server
+	log.Printf("API service starting on port %s", cfg.APIPort)
+	if err := router.Run(":" + cfg.APIPort); err != nil {
+		log.Fatalf("Failed to run API server: %v", err)
+	}
+}
---
+++ internal/repository/postgres.go
@@ -0,0 +1,63 @@
+package repository
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+type PostgreSQLRepository struct {
+	DB *sql.DB
+}
+
+func NewPostgreSQLRepository(host, port, user, password, dbname string) (*PostgreSQLRepository, error) {
+	connString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	var db *sql.DB
+	var err error
+
+	// Retry connection
+	for i := range 10 {
+		db, err = sql.Open("postgres", connString)
+		if err == nil {
+			err = db.Ping()
+			if err == nil {
+				log.Println("Successfully connected to PostgreSQL!")
+				break
+			}
+		}
+		log.Printf("Waiting for database to be ready... (%d/10) %v\n", i+1, err)
+		time.Sleep(5 * time.Second)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("could not connect to PostgreSQL after multiple attempts: %w", err)
+	}
+
+	repo := &PostgreSQLRepository{DB: db}
+	if err := repo.createTable(); err != nil {
+		return nil, fmt.Errorf("error creating table: %w", err)
+	}
+	return repo, nil
+}
+
+func (r *PostgreSQLRepository) createTable() error {
+	createTableQuery := `
+	CREATE TABLE IF NOT EXISTS vehicle_locations (
+		id SERIAL PRIMARY KEY,
+		vehicle_id VARCHAR(255) NOT NULL,
+		latitude DOUBLE PRECISION NOT NULL,
+		longitude DOUBLE PRECISION NOT NULL,
+		timestamp BIGINT NOT NULL
+	);`
+
+	_, err := r.DB.Exec(createTableQuery)
+	if err != nil {
+		return fmt.Errorf("error creating table: %w", err)
+	}
+	log.Println("Table 'vehicle_locations' checked/created successfully.")
+	return nil
+}
