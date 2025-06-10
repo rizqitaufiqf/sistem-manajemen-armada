1035e3f401bfcb8209de1c704737729df6218caa
rizqi taufiq <rizqitaufiqf@gmail.com>
2025-06-11 00:52:21 +0700

    update align

--- internal/models/models.go
+++ internal/models/models.go
@@ -15,13 +15,13 @@ type LocationPayload struct {
 }
 
 type GeofenceEvent struct {
-	VehicleID string `json:"vehicle_id"`
-	Event     string `json:"event"`
-	Location Location `json:"location"`
-	Timestamp int64 `json:"timestamp"`
+	VehicleID string   `json:"vehicle_id"`
+	Event     string   `json:"event"`
+	Location  Location `json:"location"`
+	Timestamp int64    `json:"timestamp"`
 }
 
 type Location struct {
-	Latitude float64 `json:"latitude"`
+	Latitude  float64 `json:"latitude"`
 	Longitude float64 `json:"longitude"`
 }
